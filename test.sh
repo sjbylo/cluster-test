3bba01968dd436c8f195a7a176888f026e7a707b
Stephen <stephenbylo@gmail.com>
2019-12-10 20:50:20 +0800

    Added setup steps in README, added test.sh script

---
+++ test.sh
@@ -0,0 +1,28 @@
+MY_ROUTE=$1
+MY_WAIT=0.5 
+[ "$2" ] && MY_WAIT=$2
+MAX=999999999
+[ "$3" ] && MAX=$3
+
+
+myout() {
+	T1=`echo $1 | sed "s/.*\(You have.*times\).*/\1/g"`
+	T2=`echo "$1"| egrep "^[a-zA-Z0-9_-]{40}$"`
+	echo "$T2 $T1"  
+}
+
+# Start the session
+OUT=`curl -sb .cookie $MY_ROUTE`
+myout "$OUT"
+sleep $MY_WAIT
+
+cnt=0
+while [ $cnt -lt $MAX ]
+do
+	OUT=`curl -sb .cookie $MY_ROUTE`
+	myout "$OUT"
+
+	sleep $MY_WAIT
+	let cnt=$cnt+1
+done
+
