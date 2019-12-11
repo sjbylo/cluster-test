dd3a42d303df17226085d1aa535e692863d0b15d
Stephen <stephenbylo@gmail.com>
2019-12-11 12:10:57 +0800

    Enhanced testing script

--- test.sh
+++ test.sh
@@ -1,9 +1,19 @@
 MY_ROUTE=$1
 MY_WAIT=0.5 
 [ "$2" ] && MY_WAIT=$2
-MAX=999999999
-[ "$3" ] && MAX=$3
+MAX_HITS=999999999
+[ "$3" ] && MAX_HITS=$3
 
+if [ ! "$MY_ROUTE" ]
+then
+	echo "Usage: test.sh <url> [interval] [count]"
+	echo 
+	echo "url:      the endpoint to test"
+	echo "interval: time between each hit in seconds (default is 0.5s)"
+	echo "count:    number of hits (default is no limit)"
+	
+	exit 0
+fi
 
 myout() {
 	T1=`echo $1 | sed "s/.*\(You have.*times\).*/\1/g"`
@@ -11,19 +21,25 @@ myout() {
 	echo "$T2 $T1"  
 }
 
-# Start the session
-OUT=`curl -sc .cookie $MY_ROUTE`
-myout "$OUT"
+CNT=0
 
-sleep $MY_WAIT
+if [ ! -f .cookie ]
+then
+        # Start the session
+        OUT=`curl -sc .cookie $MY_ROUTE`
+        myout "$OUT"
 
-cnt=1
-while [ $cnt -lt $MAX ]
+        sleep $MY_WAIT
+        let CNT=$CNT+1
+
+fi
+
+while [ $CNT -lt $MAX_HITS ]
 do
-	OUT=`curl -sb .cookie $MY_ROUTE`
-	myout "$OUT"
+        OUT=`curl -sb .cookie $MY_ROUTE`
+        myout "$OUT"
 
-	sleep $MY_WAIT
-	let cnt=$cnt+1
+        sleep $MY_WAIT
+        let CNT=$CNT+1
 done
 
